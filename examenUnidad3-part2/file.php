e560123c8fdb681bcbb73ad26188fb11d3716f0e
Jan Palacios <56079698+JanPalaciosC@users.noreply.github.com>
2020-09-25 11:47:34 -0600

    Add files via upload

---
+++ examenUnidad3-part2/file.php
@@ -0,0 +1,29 @@
+<?php 
+
+$mysqli = new mysqli("localhost", "root", "", "formulario");
+
+ 
+if ($mysqli->connect_errno) {
+    printf("Falló la conexión: %s\n", $mysqli->connect_error);
+    exit();
+}else{
+	
+	$email=$_POST['email'];
+	$password=$_POST['password'];
+	$year=$_POST['year'];
+
+     $sql = "INSERT INTO usuarios (email,password,year) VALUES ('$email','$password','$year')";
+ 
+     if (mysqli_query($mysqli, $sql)) {
+        echo "Agregados correctamente !";
+     } else {
+        echo "Error: " . $sql . ":-" . mysqli_error($mysqli);
+     }
+
+}
+
+
+
+$mysqli->close();
+
+?>
